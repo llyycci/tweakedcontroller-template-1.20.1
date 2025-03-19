44be3f91f7e093e50a44f1d7e5d7aa95811ab7c7
llyycci <471717882@qq.com>
2025-03-19 20:32:55 +0800

    Initial commit

---
+++ src/main/java/com/llyycci/tweakedcontroller/TweakedControllerDataGenerator.java
@@ -0,0 +1,11 @@
+package com.llyycci.tweakedcontroller;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TweakedControllerDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
